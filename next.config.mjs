69569a050577ba4f878fe1e6fac992cc56fdbc2e
enki-somer <enkisomer@gmail.com>
2024-12-25 22:30:56 +0300

    Fix configuration files for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio-future" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/enkiduv-portfolio" : "",
   trailingSlash: true,
 };
 
